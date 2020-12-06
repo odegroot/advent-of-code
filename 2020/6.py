18ebea621ee554d2e83f4ede074455d2ead80739
Oscar de Groot <oscar.de.groot@sentia.com>
2020-12-06 09:56:41 +0100

    2020 day 6

---
+++ 2020/6.py
@@ -0,0 +1,58 @@
+# https://adventofcode.com/2020/day/6
+
+import re
+import types
+
+def part_one():
+  for example in [example_1, example_2, full_input]:
+    groups = example.split('\n\n')
+    total = sum([group_count_1(group) for group in groups])
+    print(total)
+    # for group in groups:
+    #   letters = set(group.replace('\n', ''))
+    #   # people = group.splitlines()
+    #   print(letters)
+    # print('---')
+
+def group_count_1(group):
+  return len(set(group.replace('\n', '')))
+
+def part_two():
+  for example in [example_2, full_input]:
+    groups = example.split('\n\n')
+    total = sum([group_count_2(group) for group in groups])
+    print(total)
+
+def group_count_2(group):
+  people = [set(person) for person in group.splitlines()]
+  collectively = people[0].intersection(*people[1:])
+  return len(collectively)
+
+example_1 = '''
+abcx
+abcy
+abcz
+'''.strip()
+example_2 = '''
+abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b
+'''.strip()
+
+with open(f'{__file__[-4]}.input') as f:
+  full_input = f.read().strip()
+
+part_one()
+part_two()
